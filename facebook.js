9365afe021d01f844552bb8f084bb9b8c0c6092c
kryptokinght <minanshu.1998@gmail.com>
2018-03-08 02:24:19 +0530

    remove bugs

--- facebook.js
+++ facebook.js
@@ -16,10 +16,77 @@ var fs = require('fs'),
                       'Es+KCn25OKXR/FJ5fu6A6A+MptABL3r8SEjlpLc=\n'+
                       '-----END RSA PRIVATE KEY-----');
 
-var names = ["Rohit Goud"];
+var names = ["Shreyashee Sinha", "Shweta Sharma", "Apoorva Diwvedi", "Swarnali Banarjee", "Mehendi Basu"];
 var i = 0;
 var results = [];
 var completed = [];
+
+
+
+function PromiseAccumulator(accumulator, name) {
+	return accumulator.then(function(results) {
+	return nightmare
+	  .goto('https://www.facebook.com/')
+	  .wait('input[type="text"]')
+	  //.wait('span.uiIconText._5qtp')
+	  .type('input[type="text"]',name + '\u000d')
+	  .wait('a._1ii5._2yez') //waits for the first search result to load
+	  .click('a._1ii5._2yez')
+	  .wait('ul._6_7.clearfix > li:nth-child(4) > ._6-6') //wait for thephotos button to load
+		  .click('ul._6_7.clearfix > li:nth-child(4) > ._6-6') // click on photos button
+		  .wait('div._3dc :nth-child(3)') // wait for albums to load
+		  .click('div._3dc :nth-child(3) > :nth-child(1)') // click on albums button
+	  .wait(5000) //wait for the html to load otherwise it will not be able to look for table below
+	  .evaluate(() => {
+	  		function getSrc() {
+			   var promise = new Promise(function(resolve, reject) {
+			   	setTimeout(() => {
+					l = document.querySelectorAll('table._51mz > tbody > tr > td');
+					console.log(l);
+					for(let i = 0; i < l.length; i++) {
+						if(/Profile pictures/g.test(l[i].innerText)) {
+							match = i;
+							break;
+						}
+					}
+					console.log(l[match].children[0].children[0].children[0].children[0].children[0].src);
+					resolve(l[match].children[0].children[0].children[0].children[0].children[0].src);
+				}, 500);
+			   });
+			   return promise;
+			}
+		  	var l = document.querySelectorAll('table._51mz > tbody > tr > td');
+		  	let match = 0;
+		  	function pause(milliseconds) {
+				var dt = new Date();
+				while ((new Date()) - dt <= milliseconds) { /* Do nothing */ }
+			}
+		  	if(l.length == 0) { //when it shows error in loading the profile pic
+		  		var fax = [];
+		  		var s = document.getElementsByClassName('_3sz');
+		  		s[1].click();
+		  		return getSrc().then(function(done) {
+	  				return done;
+				});
+		  	}
+		  	else {
+		  		for(let i = 0; i < l.length; i++) {
+			  		if(/Profile pictures/g.test(l[i].innerText)) {
+			  			match = i;
+			  			break;
+			  		}
+		  		}
+		  		return l[match].children[0].children[0].children[0].children[0].children[0].src;
+		  	}
+		})
+	  .then(function(result){
+	    results.push(result);
+	    //names.shift();
+	    //completed.push(name);
+	    return results;
+	  });
+	});
+}
 
 fs.readFile('../fapx.txt', 'utf8', function(err, enc_data) { //fapx.txt contains the encrypted login credentials
 	var decrypted = key.decrypt(enc_data, 'utf8'); //decrypts the data in file
@@ -32,39 +99,7 @@ fs.readFile('../fapx.txt', 'utf8', function(err, enc_data) { //fapx.txt contains
 	  .type('input[type="password"]',pass + '\u000d')  // enter password
 	  .wait('span._1qv9') //waits for a random element on the page to load(input was not working)
 	  .then(() => {
-		names.reduce(function(accumulator, name) {
-		  return accumulator.then(function(results) {
-		    return nightmare
-		      .goto('https://www.facebook.com/')
-			  .wait('span.uiIconText._5qtp')
-			  .type('input[type="text"]',name + '\u000d')
-			  .wait('a._1ii5._2yez') //waits for the first search result to load
-			  .click('a._1ii5._2yez')
-			  .wait('ul._6_7.clearfix > li:nth-child(4) > ._6-6') //wait for thephotos button to load
-	  		  .click('ul._6_7.clearfix > li:nth-child(4) > ._6-6') // click on photos button
-	  		  .wait('div._3dc :nth-child(3)') // wait for albums to load
-	  		  .click('div._3dc :nth-child(3) > :nth-child(1)') // click on albums button
-			  .evaluate(() => {
-				  	let l = document.querySelectorAll('table._51mz > tbody > tr > td');
-				  	let match = 0;
-				  	for(let i = 1; i <= l.length + 2; i++) {
-				  		alert("DFGDFGD");
-				  		if(/Profile pictures/g.test(l[i].innerText)) {
-				  			match = i;
-				  			break;
-				  		}
-				  	}
-				  	return match;
-				})
-		      .then(function(result){
-		      	console.log()
-		        results.push(result);
-		        //names.shift();
-		        //completed.push(name);
-		        return results;
-		      });
-		  });
-		}, Promise.resolve([])).then(function(results){
+		names.reduce(PromiseAccumulator, Promise.resolve([])).then(function(results){
 			/*console.log("NAMES: ");
 	        console.log(names);
 		    console.log("completed: ");
@@ -73,7 +108,7 @@ fs.readFile('../fapx.txt', 'utf8', function(err, enc_data) { //fapx.txt contains
 		});
 	  })
 	  .catch(function (error) {
-	    console.error('Behenchod:', error);
+	    console.error('Errororor:', error);
 	});
 });
 /*
